dce96c1ef9042f0e8eafbe066d06be778c4901dd
Lalit Kumar <lalitchauhan1517@gmail.com>
2024-09-04 10:41:47 +0530

    updated cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -314,10 +314,7 @@ const Cart = ({ isOpen, onClose }: ModalProps): ReactElement => {
             <form
               id="userDetailsForm"
               className="flex flex-col items-center justify-between gap-10 xl:gap-12 w-full py-10"
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSubmitForm();
-              }}
+              onSubmit={handleSubmitForm}
             >
               <label className="w-full flex flex-col h-20 items-start justify-between ">
                 <div className="text-sm font-medium">Name*</div>
